fd234fd882b99d04594b1bf1351d68ef25c9670c
Code Journal <contact.codejournal@gmail.com>
2020-11-17 21:31:26 +0100

    GH-1: Project for Custom Lifecycle mappings

---
+++ 002-custom-lifecycle-mappings/final/src/test/java/io/codejournal/maven/AppTest.java
@@ -0,0 +1,16 @@
+package io.codejournal.maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
